4cfa11b414e879ce75ed686dbc7a965fdcedd3a2
Fiaz Majeed <fiazcomsats@gmail.com>
2024-08-09 15:04:17 +0500

    first commit

---
+++ Assignments/exercise_solutions.py
@@ -0,0 +1,79 @@
+#Assign a message to a variable
+message = "This is third assignment of Python"
+#Print message
+print(message)
+#Change the value of the variable to a new message
+message = "This assignment is based on 3rd class"
+#print the new message.
+print(message)
+person = "Fiaz Majeed"
+print(f"{person} is the student of PIAIC")
+# Print the person’s name in lowercase, uppercase, and title case.
+print(person.lower())
+print(person.upper())
+print(person.title())
+#Print the quote and the name of its author
+quote = "With faith, discipline and selfless devotion to duty, there is nothing worthwhile that you cannot achieve."
+author = "Quaid-e-Azam"
+print(f'{author} once said, "{quote}"')
+#Composethe message and represent it with a variable called message.
+famous_person = "Zia Khan"
+message = "PIAIC Head"
+print(f'{famous_person} is the {message}')
+#Task 7-striping 
+person_name = '\tFiaz Majeed\n'
+print(person_name)
+print(person_name.lstrip())
+print(person_name.rstrip())
+print(person_name.strip())
+#variable sum
+x = 5
+y = 10
+z = 15
+sum_xyz = x + y + z
+print ("sum is", sum_xyz)
+#variable swap
+print("Before swap")
+print ("x =", x)
+print("y =", y)
+x, y = y, x
+print("After swap")
+print ("x =", x)
+print("y =", y)
+#Assign favorite color
+color = "blue"
+print("Favorite color", color)
+color = "blue"
+print("Favorite color", color)
+color = "red"
+print("New Favorite color =", color)
+#Changing pet name
+pet_name = "Buddy"
+print ("Pet name =", pet_name)
+pet_name ="Max"
+print ("Pet name =", pet_name)
+#Observing Name Error
+x = "Sunshine"
+print(x)
+#Reassigning Score
+score = 100
+print(score)
+score = 150
+print(score)
+#City Name
+city = "Lahore"
+print(city)
+#Title Case Text
+text = "Programming language"
+print(text.title())
+# Lowercase Conversion
+text = "PrograMMing laNGuage"
+print(text.lower())
+#Uppercase Conversion
+print(text.upper())
+#Current Temperature
+temperature= 25
+print(f'The current temperature is {temperature} degrees')
+# Printing a Poem
+poem = "The sun has set, the moon is bright, \nStars are twinkling in the night. \nWhispers of the evening breeze, \nCarry secrets through the trees"
+print(poem)
---
+++ Calculator.py
@@ -0,0 +1,31 @@
+#user input
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+#perform operation
+if operation == "+":
+    result = a + b
+elif operation == "-":
+    result = a - b
+elif operation == "*":
+    result = a * b
+elif operation ==  "/":
+    if b != 0:
+        result = a / b
+    else:
+        result = "Error: division by zero not allowed"
+else:
+    result = "Invalid operation"
+
+print("output:", result)
+#varibale method
+a = 3
+b= 8
+add = a + b
+minus = a - b
+multiply = a * b
+divide = a / b
+print('addition', add)
+print("subtraction", minus)
+print("multiplication", multiply)
+print("division", divide)
